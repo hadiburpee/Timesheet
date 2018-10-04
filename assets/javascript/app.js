13661790410640d22d717850875c4f0933cbe130
Primo <bpacana@gmail.com>
2018-10-03 20:39:25 -0700

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $("#add-employee-btn").on("click", function(event) {
     event.preventDefault();
 
@@ -14,3 +15,29 @@ $("#add-employee-btn").on("click", function(event) {
       start: empStart,
       rate: empRate
     };  
+=======
+
+
+      var config = {
+        apiKey: "AIzaSyCzuJs-bPkkXMsE9dGtDkqT0LZT3v3WV0o",
+        authDomain: "test1-47778.firebaseapp.com",
+        databaseURL: "https://test1-47778.firebaseio.com",
+        projectId: "test1-47778",
+        storageBucket: "test1-47778.appspot.com",
+        messagingSenderId: "519722482230"
+      };
+      firebase.initializeApp(config);
+    
+       
+        var database = firebase.database();
+    
+    
+        var clickCounter = 0;
+
+        $("#click-button").on("click", function() {
+    
+          clickCounter++;  
+          database.ref().set({
+            clickCount: clickCounter
+          });
+>>>>>>> master
