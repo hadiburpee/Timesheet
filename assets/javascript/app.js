aa3b475140b9561136939bc72b59ec91d1409807
YNWA <joe@lioneventsgroup.com>
2018-10-03 21:19:11 -0700

    update click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,4 @@
 
-$("#add-employee-btn").on("click", function(event) {
-    event.preventDefault();
-
-    // Grabs user input
-    var empName = $("#employee-name-input").val().trim();
-    var empRole = $("#role-input").val().trim();
-    var empStart = $("#start-input").val().trim();
-    var empRate = $("#rate-input").val().trim();
-  
-    // Creates local "temporary" object for holding employee data
-    var newEmp = {
-      name: empName,
-      role: empRole,
-      start: empStart,
-      rate: empRate
-    };  
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD17_Bi4Go9Xgew2wnGqEkCtccBt7d7e30",
